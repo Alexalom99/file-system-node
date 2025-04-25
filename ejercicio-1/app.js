7c2aad14188334fb6d079b82a6fc16d6758e70d5
Alexalom99 <alexalom1999@gmail.com>
2025-04-25 12:22:33 +0200

    ex1 done

--- ejercicio-1/app.js
+++ ejercicio-1/app.js
@@ -1,6 +1,35 @@
 const fs = require('fs');
 const path = require('path');
-let dni = "";
+let dni = process.argv[2];
 
 /** Leer README */
+
+
+const data = require('./hacienda.json');
+
+// data.forEach( (person) => {
+//     console.log(person.nombre);
+// });
+
+
+
+function personInFile(dni, data) {
+    for (let person of data) {
+        if (person.dni === dni) {
+            return person;
+        }
+    }
+    return;
+}
+
+const person = personInFile(dni,data);
+if (person) {
+    const message = (`El/la contibuyente ${person.nombre} tiene notificaciones pendientes. Se enviará un mail a ${person.email}\n`);
+    fs.appendFileSync("./ejercicio-1/notificaciones.txt", message, "utf-8");
+}
+else {
+    const error = `El dni ${dni} no tiene pagos pendientes\n`;
+    fs.appendFileSync("./ejercicio-1/notificaciones.txt", error, "utf-8");
+}
+
 
